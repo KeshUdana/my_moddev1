5cae4d953220cd06ac2e45781e7043ac1c80f5d5
Keshawa_Udana <125798571+KeshUdana@users.noreply.github.com>
2024-06-16 11:48:58 +0000

    ggg

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Icon  from "./pages/Icon";
 import "./globals.css";
 import Image from "next/image"
+import Head from "next/head"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,17 +19,42 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head><link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'></link></Head>
      <body className={inter.className}>
       <main>
-      <nav className="fixed w-screen left-0 px-4 py-4 flex justify-between items-end bg-white bg-[radial-gradient(60%_120%_at_50%_50%,hsla(0,0%,100%,0)_0,rgba(252,205,238,.5)_100%)] shadow-md  z-40">
+      <header>
+      <nav className="fixed w-screen left-0 px-4 py-4 flex justify-between items-end bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0) shadow-md  z-40">
         <Image className="fixed top-0 left-0 w-40 h-18 rounded-2xl custom-transform border-9 drop-shadow-xl z-20"src="/ModdeLogo.svg" alt="logo"width={100} height={500}></Image>
         <ul className="font-serif font-light flex ml-auto px-2 text-xl text-violet-500 space-x-6">
           <li className="hover:text-violet-950 active:text-violet-700">Men</li>
           <li className="hover:text-violet-950 active:text-violet-700">Women</li>
-          <li className="hover:text-violet-950 active:text-violet-700">Trends</li>
+          <li className="hover:text-violet-950 active:text-violet-700">Docs & Usage </li>
         </ul>
-
       </nav>
+      </header>
+
+      <footer className="fixed bottom-0 w-screen left-0 px-4 py-4 flex justify-center bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))] shadow-md z-40">
+        <div className="flex flex-col items-center">
+          <ul className="font-serif font-semibold  flex justify-space between  ml-auto px-2 text-sm font-sans text-violet-500 space-x-6">
+            <li className="hover:text-violet-950 active:text-violet-700">Forge Lab</li>
+            <li className="hover:text-violet-950 active:text-violet-700">Size Guide</li>
+            <li className="hover:text-violet-950 active:text-violet-700">Contact</li>
+            <li className="hover:text-violet-950 active:text-violet-700">Privacy policy</li>
+            <li className="hover:text-violet-950 active:text-violet-700">Terms and Conditions</li>
+          </ul>
+          </div>
+          <br/>
+          <div className="flex flex-col items-center">
+          <ul className="font-serif font-semibold  flex justify-space between  ml-auto px-2 text-sm font-sans text-violet-500 space-x-6 ">
+            <li><Icon name='bx bxl-facebook-circle hover:text-purple-900'></Icon></li>
+            <li><Icon name='bx bxl-linkedin-square hover:text-purple-900'></Icon></li>
+            <li><Icon name='bx bxl-youtube hover:text-purple-900'></Icon></li>
+            <li><Icon name='bx bxl-instagram hover:text-purple-900'></Icon></li>
+
+          </ul>
+        </div>
+      </footer>
+
       {children}</main></body>
     </html>
   );
--- app/pages/home.tsx
+++ app/pages/home.tsx
@@ -1,22 +1,36 @@
 import React from "react";
 import '../globals.css';
+import Icon  from "./Icon";
 import Image from 'next/image';
 
 function Homepage() {
     return (
         <> 
-        <section className="h-screen w-screen px-5 py-24 bg-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#63e_100%)] md:w-screen">
-        <div className="flex justify-start">
-    <div className=" inline w-full max-w-screen-lg p-6 bg-purple-100 border border-gray-200 rounded-lg">
-        <div className="w-1/3 max-w-screen-lg p-6 bg-transparent border-dashed border-2 border-gray-200 rounded-lg ">
-      <h5 className="mb-2 text-3xl text-center font-bold font-serif tracking-tight text-purple-950">Enter desired product</h5>
-      <a href="#" className="inline-flex justify-center items-center px-3 py-2 text-lg font-medium text-white bg-purple-600 rounded-lg hover:bg-fuchsia-800 focus:ring-4 focus:outline-none focus:ring-fuchsia-300 dark:bg-purple-700 dark:hover:bg-purple-950">
-        Browse Files
-      </a></div>
-            </div>
-            </div>
-        </section>
+            <section className="h-screen w-screen px-5 py-24 otate-180 transform bg-white bg-[radial-gradient(60%_120%_at_50%_50%,hsla(0,0%,100%,0)_0,rgba(252,205,238,.5)_100%)] md:w-screen">
+  <div className="flex justify-start items-center container mx-auto px-4 py-8 h-full">
+    <div className="inline max-w-screen-xl p-6 rounded-lg bg-white/0 shadow-xl backdrop-blur-xl">
+    <h5 className="mb-2 text-3xl text-center font-bold font-serif tracking-tight text-purple-950">Enter desired product</h5>
+    <div className="max-w-sm p-6 bg-purple-100 border-dashed border-2 border-purple-950 rounded-lg">
+        <div className="flex justify-center">
+            <ul>
+            <li className="text-xl font-sans font-semibold text-purple-950">Drag and drop image</li>
+            <li className="text-center text-2xl font-sans font-semibold text-purple-950">OR</li>
+            </ul>
+          </div>
+          <Icon name='bx bxs-plus-circle bx-md text-purple-700 flex justify-center hover:text-purple-950' />
+
+        <br />
+        <div className="flex justify-center">
+          <a href="#" className="inline-flex justify-center items-center px-3 py-2 text-lg font-medium text-white bg-purple-600 rounded-lg hover:bg-fuchsia-800 focus:ring-4 focus:outline-none focus:ring-fuchsia-300 dark:bg-purple-700 dark:hover:bg-purple-950">
+            Browse Files
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
         </>
-    );
+    );                                                                                      
 }
 export default Homepage;
